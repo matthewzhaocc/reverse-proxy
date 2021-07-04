eff71253b21335b356a135e8c0e14819b1d1559c
Matthew <nicetry.ihatemyself.xyz>
2021-07-04 15:42:41 -0700

    xd

--- app.go
+++ app.go
@@ -6,21 +6,52 @@ import (
 	"net/url"
 	"github.com/gorilla/mux"
 	"encoding/json"
+	"path/filepath"
+	"io/ioutil"
 )
-
+var config ReverseProxyConfiguration
 func init() {
-	file, err :=
+	configFile, err := filepath.Abs("./config.json")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	value, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err.Error())
+	}
+	json.Unmarshal(value, &config)
 }
 
 func main() {
-	mux := mux.NewRouter()
-	rProxyBackend := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "https",
-		Host: "aws.amazon.com",
-	})
-	route := mux.Host("sus.local")
-	route.HandlerFunc(rProxyBackend.ServeHTTP)
+	muxRouter := mux.NewRouter()
+	for _, frontend := range config.Frontends {
+		route := muxRouter.Host(frontend.Hostname)
+		for _, backend := range config.Hosts {
+			if backend.FrontendHostName == frontend.Hostname {
+				route.HandlerFunc(httputil.NewSingleHostReverseProxy(&url.URL{
+					Scheme: backend.Scheme,
+					Host: backend.Host,
+				}).ServeHTTP)
+			}
+		}
+	} 
 	
-	http.ListenAndServe(":3000", mux)
+	http.ListenAndServe(":3000", muxRouter)
 	
+}
+
+type ReverseProxyConfiguration struct {
+	Hosts []ReverseProxyHost `json:"hosts"`
+	Frontends []Frontend `json:"frontends"`
+}
+
+type ReverseProxyHost struct {
+	Scheme string `json:"scheme"`
+	Host string	`json:"host"`
+	FrontendHostName string `json:"frontend_hostname"`
+}
+
+type Frontend struct {
+	Hostname string `json:"hostname"`
 }
